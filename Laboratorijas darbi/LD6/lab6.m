a393e24dc3ede4da9c9f673b0db3b1b9c89cf5ac
user <elvira113@mail.ru>
2018-04-26 09:50:41 +0300

    26_04_2018_9_50

---
+++ Laboratorijas darbi/LD6/lab6.m
@@ -0,0 +1,482 @@
+t=0:0.1:1;
+y = lab_3_demo_fun(t);
+{Undefined function or variable 'lab_3_demo_fun'.
+} 
+y = lab_3_fun(t);
+{Undefined function or variable 'lab_3_fun'.
+} 
+y = LD_3_demo_fun(t);
+{Undefined function or variable 'y_noise'.
+
+Error in <a href="matlab:matlab.internal.language.introspective.errorDocCallback('LD_3_demo_fun', '/home/user/MATLAB/Laboratorijas darbi/LD3/LD_3_demo_fun.m', 19)" style="font-weight:bold">LD_3_demo_fun</a> (<a href="matlab: opentoline('/home/user/MATLAB/Laboratorijas darbi/LD3/LD_3_demo_fun.m',19,0)">line 19</a>)
+y=[y_zero,y_noise,y_saw,y_sin,y_const];
+} 
+y = LD_3_demo_fun(t);
+plot(t,y)
+{Error using <a href="matlab:matlab.internal.language.introspective.errorDocCallback('plot')" style="font-weight:bold">plot</a>
+Vectors must be the same length.
+} 
+size(t)
+
+ans =
+
+     1    11
+
+size(y)
+
+ans =
+
+     1   601
+
+y = LD_3_demo_fun(t);
+size(y)
+
+ans =
+
+     1    11
+
+size(t)
+
+ans =
+
+     1    11
+
+plot(t,y)
+edit lab_6.m
+lab_6
+
+xvid3a =
+
+    0.6151
+
+lab_6
+
+xvid3a =
+
+    0.6151
+
+
+xvid3b =
+
+    0.6151
+
+lab_6
+
+xvid3a =
+
+    0.6151
+
+
+xvid3b =
+
+    0.2818
+
+lab_6
+
+xvid3a =
+
+    0.6151
+
+
+xvid3b =
+
+    0.5818
+
+lab_6
+
+xvid3a =
+
+    0.6151
+
+
+xvid3bb =
+
+    0.5818
+
+
+xvid3b =
+
+    0.5818
+
+lab_6
+
+xvid3a =
+
+    0.6151
+
+
+xvid3bb =
+
+    0.5818
+
+
+xvid3b =
+
+    0.5818
+
+
+h =
+
+    0.1000
+
+{Subscript indices must either be real positive integers or logicals.
+
+Error in <a href="matlab:matlab.internal.language.introspective.errorDocCallback('lab_6', '/home/user/MATLAB/Laboratorijas darbi/LD3/lab_6.m', 15)" style="font-weight:bold">lab_6</a> (<a href="matlab: opentoline('/home/user/MATLAB/Laboratorijas darbi/LD3/lab_6.m',15,0)">line 15</a>)
+xvid3c = 1/(N-1)*sum(sig(t((1:end-1)+(h/2))))
+} 
+lab_6
+
+xvid3a =
+
+    0.6151
+
+
+xvid3bb =
+
+    0.5818
+
+
+xvid3b =
+
+    0.5818
+
+
+h =
+
+    0.1000
+
+{Subscript indices must either be real positive integers or logicals.
+
+Error in <a href="matlab:matlab.internal.language.introspective.errorDocCallback('lab_6', '/home/user/MATLAB/Laboratorijas darbi/LD3/lab_6.m', 15)" style="font-weight:bold">lab_6</a> (<a href="matlab: opentoline('/home/user/MATLAB/Laboratorijas darbi/LD3/lab_6.m',15,0)">line 15</a>)
+xvid3c = 1/(N-1)*sum(sig(t((1:end-1)+(h/2))))
+} 
+lab_6
+
+xvid3a =
+
+    0.6151
+
+
+xvid3bb =
+
+    0.5818
+
+
+xvid3b =
+
+    0.5818
+
+
+h =
+
+    0.1000
+
+
+h =
+
+    4.9033
+
+{Subscript indices must either be real positive integers or logicals.
+
+Error in <a href="matlab:matlab.internal.language.introspective.errorDocCallback('lab_6', '/home/user/MATLAB/Laboratorijas darbi/LD3/lab_6.m', 16)" style="font-weight:bold">lab_6</a> (<a href="matlab: opentoline('/home/user/MATLAB/Laboratorijas darbi/LD3/lab_6.m',16,0)">line 16</a>)
+xvid3c = 1/(N-1)*sum(sig(t((1:end-1)+(h/2))))
+} 
+lab_6
+{Error: <a href="matlab: opentoline('/home/user/MATLAB/Laboratorijas darbi/LD3/lab_6.m',16,45)">File: lab_6.m Line: 16 Column: 45
+</a>Expression or statement is incorrect--possibly unbalanced (, {, or [.
+} 
+lab_6
+
+xvid3a =
+
+    0.6151
+
+
+xvid3bb =
+
+    0.5818
+
+
+xvid3b =
+
+    0.5818
+
+
+h =
+
+    4.9033
+
+{Subscript indices must either be real positive integers or logicals.
+
+Error in <a href="matlab:matlab.internal.language.introspective.errorDocCallback('lab_6', '/home/user/MATLAB/Laboratorijas darbi/LD3/lab_6.m', 15)" style="font-weight:bold">lab_6</a> (<a href="matlab: opentoline('/home/user/MATLAB/Laboratorijas darbi/LD3/lab_6.m',15,0)">line 15</a>)
+xvid3c = 1/(N-1)*sum(sig(t((1:end-1)+(h/2))))
+} 
+lab_6
+
+xvid3a =
+
+    0.6151
+
+
+xvid3bb =
+
+    0.5818
+
+
+xvid3b =
+
+    0.5818
+
+
+h =
+
+    0.0017
+
+{Subscript indices must either be real positive integers or logicals.
+
+Error in <a href="matlab:matlab.internal.language.introspective.errorDocCallback('lab_6', '/home/user/MATLAB/Laboratorijas darbi/LD3/lab_6.m', 15)" style="font-weight:bold">lab_6</a> (<a href="matlab: opentoline('/home/user/MATLAB/Laboratorijas darbi/LD3/lab_6.m',15,0)">line 15</a>)
+xvid3c = 1/(N-1)*sum(sig(t((1:end-1)+(h/2))))
+} 
+lab_6
+
+xvid3a =
+
+    0.6151
+
+
+xvid3bb =
+
+    0.5818
+
+
+xvid3b =
+
+    0.5818
+
+
+h =
+
+    0.0017
+
+
+xvid3c =
+
+    0.6152
+
+lab_6
+
+xvid3a =
+
+    0.6151
+
+
+xvid3bb =
+
+    0.5818
+
+
+xvid3b =
+
+    0.5818
+
+
+h =
+
+    0.1000
+
+
+xvid3c =
+
+    0.6183
+
+lab_6
+{Error: <a href="matlab: opentoline('/home/user/MATLAB/Laboratorijas darbi/LD3/lab_6.m',18,62)">File: lab_6.m Line: 18 Column: 62
+</a>Expression or statement is incorrect--possibly unbalanced (, {, or [.
+} 
+lab_6
+
+xvid3a =
+
+    0.6151
+
+
+xvid3bb =
+
+    0.5818
+
+
+xvid3b =
+
+    0.5818
+
+
+h =
+
+    0.1000
+
+
+xvid3c =
+
+    0.6183
+
+
+xvid4 =
+
+    0.6318
+
+lab_6
+
+xvid3a =
+
+    0.6151
+
+
+xvid3bb =
+
+    0.5818
+
+
+xvid3b =
+
+    0.5818
+
+
+h =
+
+    0.1000
+
+
+xvid3c =
+
+    0.6183
+
+
+xvid4 =
+
+    0.5984
+
+lab_6
+
+xvid3a =
+
+    0.6151
+
+
+xvid3bb =
+
+    0.5818
+
+
+xvid3b =
+
+    0.5818
+
+
+h =
+
+    0.1000
+
+
+xvid3c =
+
+    0.6183
+
+
+xvid4 =
+
+    0.5984
+
+lab_6
+
+xvid3a =
+
+    0.6151
+
+
+xvid3bb =
+
+    0.5818
+
+
+xvid3b =
+
+    0.5818
+
+
+h =
+
+    0.1000
+
+
+xvid3c =
+
+    0.6183
+
+
+xvid4 =
+
+    0.5984
+
+lab_6
+
+xvid3a =
+
+    0.6151
+
+
+xvid3bb =
+
+    0.5818
+
+
+xvid3b =
+
+    0.5818
+
+
+h =
+
+    0.1000
+
+
+xvid3c =
+
+    0.6183
+
+
+xvid4 =
+
+    0.5929
+
+lab_6
+
+xvid3a =
+
+    0.6151
+
+
+xvid3bb =
+
+    0.5818
+
+
+xvid3b =
+
+    0.5818
+
+
+h =
+
+    0.1000
+
+
+xvid3c =
+
+    0.6183
+
+
+xvid4 =
+
+    0.5984
+
+diary off
